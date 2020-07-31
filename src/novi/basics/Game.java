d05074cb864f6ed76d2649a1795e97cfa133325f
Dennis Haverhals <dennis.haverhals@noviuniversity.com>
2020-07-31 16:12:03 +0200

    github

--- src/novi/basics/Game.java
+++ src/novi/basics/Game.java
@@ -16,11 +16,6 @@ public class Game {
 
     //constructor
     public Game(Player player1, Player player2) {
-        // speelbord opslaan (1 - 9)
-        // uitleg: omdat we altijd met een bord starten met 9 getallen, kunnen we het Tic Tac Toe bord ook direct een waarde geven
-        // zie demo project code voor de andere manier met de for loop
-        //board = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
         board = new Field[9];
 
         for (int fieldIndex = 0; fieldIndex < 9; fieldIndex++) {
@@ -39,78 +34,111 @@ public class Game {
 
     public void play() {
 
-        // -- vanaf hier gaan we het spel opnieuw spelen met dezelfde spelers (nadat op het eind keuze 1 gekozen is)
-
-        // speelbord tonen
         printBoard();
 
-        // starten met de beurt (maximaal 9 beurten)
-        for (int round = 0; round < maxRounds; round++) {
+        int round = 1;
+        while(round <= maxRounds) {
 
-            // naam van de actieve speler opslaan
             String activePlayerName = activePlayer.getName();
-
-            // actieve speler vragen om een veld te kiezen (1 - 9)
             System.out.println(activePlayerName + ", please choose a field");
 
-            //setField();
-            // gekozen veld van de actieve speler opslaan
-            int chosenField = PLAYERINPUT.nextInt();
-            int chosenIndex = chosenField - 1;
+            try {
+                int chosenField = PLAYERINPUT.nextInt();
+                int chosenIndex = chosenField - 1;
 
-            // als het veld bestaat
-            if(chosenIndex < 9 && chosenIndex >= 0) {
-                //als het veld leeg is, wanneer er geen token staat
-                if(board[chosenIndex].isEmpty()) {
-                    // wanneer de speler een token op het bord kan plaatsen
+                if (setField(chosenIndex)) {
 
-                    // de token van de actieve speler op het gekozen veld plaatsen
-                    char token = activePlayer.getToken();
-                    board[chosenIndex].setToken(token);
-
-                    // het nieuwe speelbord tonen
                     printBoard();
-                    // als het spel gewonnen is
-                    // tonen wie er gewonnen heeft (de actieve speler)
-                    // de actieve speler krijgt een punt
-                    // de scores van de spelers tonen
 
-                    // wanneer we in de laatste beurt zijn en niemand gewonnen heeft
-                    // aantal keer gelijk spel ophogen
-                    // aantal keer gelijk spel tonen
-
-                    // de beurt doorgeven aan de volgende speler (van speler wisselen)
-                    // als de actieve speler, speler 1 is:
-                    //changePlayer();
-                    if(activePlayer == player1) {
-                        // maak de actieve speler, speler 2
-                        activePlayer = player2;
-                    }
-                    // anders
-                    else {
-                        // maak de actieve speler weer speler 1
-                        activePlayer = player1;
+                    if (isGameWon()) {
+                        winGame();
+                        break;
+                    } else if (isLastRound(round)) {
+                        drawGame();
                     }
 
-                } //of al bezet is
-                else {
-                    maxRounds++;
-                    System.out.println("this field is not available, choose another");
+                    changePlayer();
+                    round++;
                 }
+            } catch (Exception e) {
+                handleError("Please choose one of the available fields (1-9)");
             }
-            // als het veld niet bestaat
-            else {
-                // het mamimale aantal beurten verhogen
-                maxRounds++;
-                // foutmelding tonen aan de speler
-                System.out.println("the chosen field does not exist, try again");
-            }
-
-
-
-            // -- terug naar het begin van de volgende beurt
-
         }
+    }
+
+    private void handleError(String message) {
+        System.out.println(message);
+        PLAYERINPUT.nextLine();
+    }
+
+    public boolean isGameWon() {
+        int[][] winconditions = {
+                {0,1,2},
+                {3,4,5},
+                {6,7,8},
+                {0,3,6},
+                {1,4,7},
+                {2,5,8},
+                {0,4,8},
+                {2,4,6}
+        };
+
+        for (int[] wincondition: winconditions) {
+            if( board[wincondition[0]].getToken() == board[wincondition[1]].getToken() &&
+                board[wincondition[1]].getToken() == board[wincondition[2]].getToken()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void winGame() {
+        activePlayer.addScore();
+        System.out.println(activePlayer.getName()+ " has won!");
+        System.out.println("Score " + player1.getName() + ": " + player1.getScore());
+        System.out.println("Score " + player2.getName() + ": " + player2.getScore());
+    }
+
+    public boolean isLastRound(int round) {
+        return round == maxRounds - 1;
+    }
+
+    public void drawGame() {
+        drawCount++;
+        System.out.println("It's a draw!");
+        System.out.println("There have been " + drawCount + " draws");
+    }
+
+    public void changePlayer() {
+        if (activePlayer == player1) {
+            // maak de actieve speler, speler 2
+            activePlayer = player2;
+        }
+        // anders
+        else {
+            // maak de actieve speler weer speler 1
+            activePlayer = player1;
+        }
+    }
+
+    public boolean setField(int chosenIndex) {
+        boolean result = false;
+        if(chosenIndex < 9 && chosenIndex >= 0) {
+            //als het veld leeg is, wanneer er geen token staat
+            if (board[chosenIndex].isEmpty()) {
+                // wanneer de speler een token op het bord kan plaatsen
+
+                // de token van de actieve speler op het gekozen veld plaatsen
+                char token = activePlayer.getToken();
+                board[chosenIndex].setToken(token);
+                result = true;
+            } else {
+                handleError("this field is not available, choose another");
+            }
+        } else {
+            handleError("the chosen field does not exist, try again");
+        }
+        return result;
     }
 
     public void printBoard() {
--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -8,33 +8,50 @@ public class Main {
     public static void main(String[] args) {
 
         // -- vanaf hier gaan we het spel opnieuw spelen met andere spelers (nadat op het eind  keuze 2 gekozen is)
+        boolean resetPlayers = true;
+        while (resetPlayers) {
+            // de 1e speler om zijn naam vragen
+            System.out.println("Player 1, what is your name?");
+            // de naam van de 1e speler opslaan
+            String player1Name = PLAYERINPUT.next();
+            Player player1 = new Player(player1Name, 'x');
 
-        // de 1e speler om zijn naam vragen
-        System.out.println("Player 1, what is your name?");
-        // de naam van de 1e speler opslaan
-        String player1Name = PLAYERINPUT.next();
+            // de 2e speler om zijn naam vragen
+            System.out.println("Player 2, what is your name?");
+            // de naam van de 2e speler opslaan
+            String player2Name = PLAYERINPUT.next();
+            Player player2 = new Player(player2Name, 'o');
 
-        Player player1 = new Player(player1Name,'x');
+            boolean rematch = true;
+            while (rematch) {
+                Game game = new Game(player1, player2);
+                game.play();
 
-        // de 2e speler om zijn naam vragen
-        System.out.println("Player 2, what is your name?");
-        // de naam van de 2e speler opslaan
-        String player2Name = PLAYERINPUT.next();
-        Player player2 = new Player(player2Name,'o');
+                // vragen of de spelers nog een keer willen spelen
+                System.out.println("What do you want to do next?");
+                //1: nog een keer spelen
+                System.out.println("1: Rematch with same players");
+                //2: van spelers wisselen
+                System.out.println("2: Change Players");
+                //3: afsluiten
+                System.out.println("3: Exit");
 
-        Game game = new Game(player1, player2);
-        game.play();
+                // speler keuze opslaan
+                int choice = PLAYERINPUT.nextInt();
+                switch (choice) {
+                    case 2: rematch = false;
+                            break;
 
-        // vragen of de spelers nog een keer willen spelen
-            //1: nog een keer spelen
-            //2: van spelers wisselen
-            //3: afsluiten
+                    case 3: rematch = false;
+                            resetPlayers = false;
+                            break;
+                }
 
-            // speler keuze opslaan
+                // bij keuze 1: terug naar het maken van het bord
+                // bij keuze 2: terug naar de start van de applicatie en het vragen van spelernamen
+                // bij keuze 3: het spel en de applicatie helemaal afsluiten
 
-            // bij keuze 1: terug naar het maken van het bord
-            // bij keuze 2: terug naar de start van de applicatie en het vragen van spelernamen
-            // bij keuze 3: het spel en de applicatie helemaal afsluiten
-
+            }
+        }
     }
 }
